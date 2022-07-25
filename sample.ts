0edad8998e4022d9838f2f9512b746a9b21e9be5
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-07-25 16:40:55 +0200

    Cleaning sample.ts

--- sample.ts
+++ sample.ts
@@ -1,4 +1,3 @@
-import { assert } from 'chai'
 import { Constructor, Match, Product } from './src'
 
 interface Dog extends Product<"Dog"> {
@@ -55,93 +54,6 @@ console.log("My only friends : ", cats)
 console.log("Pedant pedestrians : ", horses)
 
 console.log("choosen one : ", nameOrTitle)
-
-export const deepEquals = (obj1: object, obj2: object): boolean => {
-  if (Object.is(obj1, obj2))
-    return true
-  
-  const keys1 = Object.keys(obj1)
-  const keys2 = Object.keys(obj2)
-
-  if (keys1.length !== keys2.length)
-    return false
-
-  if (!(keys1.every(key1 => keys2.find(key2 => key1 === key2) && keys2.every(key2 => keys1.find(key1 => key2 === key1)))))
-    return false
-  
-  const entries1 = Object.entries(obj1)
-  const entries2 = Object.entries(obj2)
-
-  entries1.sort((a, b) => a[0].localeCompare(b[0]))
-  entries2.sort((a, b) => a[0].localeCompare(b[0]))
-
-  
-  for (let i = 0; i < keys1.length; i++) {
-    if (typeof entries1[i][1] !== typeof entries2[i][1])
-      return false
-
-    if(Number.isNaN(entries1[i][1]) || Number.isNaN(entries2[i][1]))
-      return false
-
-    if(entries1[i][1] === null && entries2[i][1] === null)
-      return true
-   
-    if(entries1[i][1] === null && entries2[i][1] !== null)
-      return false
-
-    if(entries1[i][1] !== null && entries2[i][1] === null)
-      return false
-      
-    switch (typeof entries1[i][1]) {
-      case 'object':
-        if (!deepEquals(entries1[i][1], entries2[i][1]))
-          return false
-        break;
-      
-      default:
-        if(entries1[i][1] !== entries2[i][1])
-          return false
-        break;
-    }
-    
-  }
-
-  return true
-}
-
-export const doesMatch = (target: object, value: object): boolean => {
-
-  const valueKeys = Object.keys(value)
-  const commonKeys = Object.keys(target).filter(value => valueKeys.includes(value))
-  const targetKV = Object.entries(target)
-  const valueKV = Object.entries(value)
-
-  if (targetKV.length === 0)
-    return true
-  else if (commonKeys.length === 0)
-    return false
-  else {
-    const checkTarget: [string, unknown][]= commonKeys.map(commonKey => {
-      return valueKV.find(element => element[0] === commonKey) as [string, unknown] //commonKeys contains all the common keys so each array must contain the key we are looking for
-    })
-
-    const checkValue: [string, unknown][] = commonKeys.map(commonKey => {
-      return targetKV.find(element => element[0] === commonKey) as [string, unknown]
-    })
-
-    checkTarget.sort((a, b) => a[0].localeCompare(b[0]))
-    checkValue.sort((a, b) => a[0].localeCompare(b[0]))
-
-    return deepEquals(checkTarget, checkValue)
-
-  }
-}
-
-const matchCase = <Target>(target: Target) => <T extends Partial<Target>, Return>(handler: (value: T) => Return) => (value: T): Return | null =>
-  doesMatch(target as unknown as object, value) ? handler(value) : null
-
-const defaultCase = <T, Return>(handler: (value: T) => Return) => (value: T): Return | null =>
-  handler(value)
 
 /*
 const trueCats = animals.filter(matchAnimal({
