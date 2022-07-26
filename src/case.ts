69d501bbd52b5fdc43809326b174f86a7b620064
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-07-26 15:37:27 +0200

    Export matchCase and defaultCase

--- src/case.ts
+++ src/case.ts
@@ -1,7 +1,7 @@
 import { doesMatch } from "./does-match"
 
-const matchCase = <Target>(target: Target) => <T extends Partial<Target>, Return>(handler: (value: T) => Return) => (value: T): Return | null =>
+export const matchCase = <Target>(target: Target) => <T extends Partial<Target>, Return>(handler: (value: T) => Return) => (value: T): Return | null =>
   doesMatch(target as unknown as object, value) ? handler(value) : null
 
-const defaultCase = <T, Return>(handler: (value: T) => Return) => (value: T): Return | null =>
+export const defaultCase = <T, Return>(handler: (value: T) => Return) => (value: T): Return | null =>
   handler(value)
