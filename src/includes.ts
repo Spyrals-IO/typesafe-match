0d8b274e4c4ba3734e3c540d7962b9557182ebc9
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-08-02 15:44:26 +0200

    Managing types and removing returns

--- src/includes.ts
+++ src/includes.ts
@@ -1,6 +1,2 @@
-export const includes = (array: Array<any>, searchElement: any): boolean => {
-  if (array.findIndex((element) => element === searchElement) === -1) {
-    return false
-  }  
-  return true
-}
+export const includes = <T>(array: Array<T>, searchElement: T): boolean =>
+  array.findIndex((element) => element === searchElement) !== -1
