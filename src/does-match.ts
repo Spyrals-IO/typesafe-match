c5d861a8df860d4aa13631bca1d602bed8794cd4
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-08-04 15:31:03 +0200

    checkCommonKeys()

--- src/does-match.ts
+++ src/does-match.ts
@@ -8,19 +8,19 @@ export const doesMatch = (target: object, value: object): boolean => {
   const commonKeys = Object.keys(target).filter(value => includes(valueKeys, value))
   const targetKV = entries(target)
   const valueKV = entries(value)
+  const checkCommonKeys = (entree: any[][]):[string, unknown][] => {
+    return commonKeys.map(commonKey => 
+      entree.find(element => element[0] === commonKey) as [string, unknown] //commonKeys contains all the common keys so each array must contain the key we are looking for
+    )
+  }
 
   if (targetKV.length === 0)
     return true
   if (commonKeys.length === 0)
     return false
   
-  const checkTarget: [string, unknown][]= commonKeys.map(commonKey => 
-    valueKV.find(element => element[0] === commonKey) as [string, unknown] //commonKeys contains all the common keys so each array must contain the key we are looking for
-  )
-
-  const checkValue: [string, unknown][] = commonKeys.map(commonKey => 
-    targetKV.find(element => element[0] === commonKey) as [string, unknown]
-  )
+  const checkTarget = checkCommonKeys(valueKV)
+  const checkValue = checkCommonKeys(targetKV)
 
   checkTarget.sort((a, b) => a[0].localeCompare(b[0]))
   checkValue.sort((a, b) => a[0].localeCompare(b[0]))
