d21fca5d6759b27521b14df38cc2fc4f43ad520b
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-08-29 16:45:00 +0200

    Removing unused code

--- src/Match.ts
+++ src/Match.ts
@@ -15,10 +15,4 @@ type NamedMatches<T extends ReadonlyArray<{__type: string}>, Return> =
   T[1] extends undefined ? NamedMatch<T[0], Return> : NamedMatch<T[0], Return> & NamedMatches<List.Tail<T>, Return>
 
 type NamedMatch<Param extends {__type: string}, Return> =
-  { [key in Param["__type"]]: PreciseMatch<Param, Return> | ReadonlyArray<PreciseMatch<Param, Return>>}
-
-type PreciseMatch<Param, Return> = (match: Param) => Return
-//type PreciseMatch<Param, Return> = <S extends Partial<Param>>(match: Param, selector?: S) => Return
-
-//type Selector<Param> = {[key in keyof Param]?: Param[key]}
-//type Actual<Param, Selector> = Selector extends infer S ? Param & S : Param
+  { [key in Param["__type"]]: (match: Param) => Return }
