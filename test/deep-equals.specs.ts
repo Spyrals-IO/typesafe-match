527d0997a060e9a0a9d02878822c5962d1f32708
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-08-29 12:40:15 +0200

    Replacing indents by spaces

--- test/deep-equals.specs.ts
+++ test/deep-equals.specs.ts
@@ -4,13 +4,13 @@ import { deepEquals } from '../src/deep-equals'
 import { twoObjectsEqualsInContent, twoObjectsInequalsInContent, valueAndTargetSameKeysDifferentValues } from './generators'
 
 describe('deepEquals', () => {
-	it('should return true when two objects are equals in content order insensitive', () => {
-		fc.assert(fc.property(twoObjectsEqualsInContent, ([a, b]) => {deepEquals(a, b)}), {numRuns: 1000})
-	}).timeout(100_000)
-	it('should return false when two objects are inequals in content', () => {
-		fc.assert(fc.property(twoObjectsInequalsInContent, ([a, b]) => {!deepEquals(a, b)}), {numRuns: 1000})
-	}).timeout(100_000)
-	it('should return false when two objects are inequals in values but same keys', () => {
-		fc.assert(fc.property(valueAndTargetSameKeysDifferentValues, ([a, b]) => {!deepEquals(a, b)}), {numRuns: 1000})
-	}).timeout(100_000)
+  it('should return true when two objects are equals in content order insensitive', () => {
+    fc.assert(fc.property(twoObjectsEqualsInContent, ([a, b]) => {deepEquals(a, b)}), {numRuns: 1000})
+  }).timeout(100_000)
+  it('should return false when two objects are inequals in content', () => {
+    fc.assert(fc.property(twoObjectsInequalsInContent, ([a, b]) => {!deepEquals(a, b)}), {numRuns: 1000})
+  }).timeout(100_000)
+  it('should return false when two objects are inequals in values but same keys', () => {
+    fc.assert(fc.property(valueAndTargetSameKeysDifferentValues, ([a, b]) => {!deepEquals(a, b)}), {numRuns: 1000})
+  }).timeout(100_000)
 })
