16a4ec85b37017150e8e650eed7a2a148e86a4b0
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-08-29 16:35:13 +0200

    Removing useless sort and changing types

--- src/does-match.ts
+++ src/does-match.ts
@@ -6,9 +6,9 @@ export const doesMatch = (target: object, value: object): boolean => {
 
   const valueKeys = Object.keys(value)
   const commonKeys = Object.keys(target).filter(value => includes(valueKeys, value))
-  const targetKV = entries(target)
-  const valueKV = entries(value)
-  const checkCommonKeys = (entree: any[][]):[string, unknown][] => {
+  const targetKV = entries(target as Record<string, unknown>)
+  const valueKV = entries(value as Record<string, unknown>)
+  const checkCommonKeys = (entree: ReadonlyArray<ReadonlyArray<unknown>>):[string, unknown][] => {
     return commonKeys.map(commonKey => 
       entree.find(element => element[0] === commonKey) as [string, unknown] //commonKeys contains all the common keys so each array must contain the key we are looking for
     )
@@ -21,9 +21,6 @@ export const doesMatch = (target: object, value: object): boolean => {
   
   const checkTarget = checkCommonKeys(valueKV)
   const checkValue = checkCommonKeys(targetKV)
-
-  checkTarget.sort((a, b) => a[0].localeCompare(b[0]))
-  checkValue.sort((a, b) => a[0].localeCompare(b[0]))
-
-  return deepEquals(checkTarget, checkValue)
+  
+  return deepEquals(checkTarget as unknown as Record<string, unknown>, checkValue as unknown as Record<string, unknown>)
 }
