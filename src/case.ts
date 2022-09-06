f762a17a4305f37a0a1367838223bccb20f677cb
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-09-06 16:16:49 +0200

    Optionnal parameter condition

--- src/case.ts
+++ src/case.ts
@@ -9,10 +9,10 @@ export type Case<Product, Return> = [(value: Product) => boolean, (value: Produc
 
 export const matchCase = <Target extends object, Product extends object>(
   target: Target,
-  condition: MatchCondition<Product>
+  condition?: MatchCondition<Product>
 ) => <Return>(
   handler: (value: Product) => Return
-): Case<Product, Return> => [(value: Product) => condition(value) && doesMatch(target, value), handler]
+): Case<Product, Return> => [(value: Product) => typeof condition !== "undefined" ? condition(value) && doesMatch(target, value) : doesMatch(target, value), handler]
 
 export const defaultCase = <Product extends object, Return>(
   condition: MatchCondition<Product>,
