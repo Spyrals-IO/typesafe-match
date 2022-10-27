95c5e81565ddbe1b33cc73142df0fead4cec33f7
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-10-27 15:31:50 +0200

    Removing useless returns

--- src/isMatcher.ts
+++ src/isMatcher.ts
@@ -1,21 +1,20 @@
 import { values } from './values'
 
-const isValidator = (currentValue: any): currentValue is any => {
-  return typeof currentValue() === "boolean" && currentValue.length === 1
-}
+const isValidator = (currentValue: any): currentValue is any =>
+  typeof currentValue() === "boolean" && currentValue.length === 1
 
-const isHandler = (currentValue: any): currentValue is any => {
-  return typeof currentValue === "function" && currentValue.length <= 1
-}
 
-const isCase = (currentValue: any): currentValue is any =>  {
-  return Array.isArray(currentValue) && currentValue.length === 2 ? isValidator(currentValue[0]) && isHandler(currentValue[1]) : false
-}
+const isHandler = (currentValue: any): currentValue is any => 
+  typeof currentValue === "function" && currentValue.length <= 1
 
-const isCaseOrFunction = (currentValue: any): boolean => {
-  return Array.isArray(currentValue) ? currentValue.every(isCase) : typeof currentValue === "function" && currentValue.length <= 1
-}
 
-export const isMatcher = (matcherOrMatchOn: any): matcherOrMatchOn is any => {
-  return values(matcherOrMatchOn).every(isCaseOrFunction)
-}
+const isCase = (currentValue: any): currentValue is any =>  
+  Array.isArray(currentValue) && currentValue.length === 2 ? isValidator(currentValue[0]) && isHandler(currentValue[1]) : false
+
+
+const isCaseOrFunction = (currentValue: any): boolean => 
+  Array.isArray(currentValue) ? currentValue.every(isCase) : typeof currentValue === "function" && currentValue.length <= 1
+
+
+export const isMatcher = (matcherOrMatchOn: any): matcherOrMatchOn is any =>
+  values(matcherOrMatchOn).every(isCaseOrFunction)
